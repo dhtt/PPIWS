dca0ef656339ccd71f4d912fb0c04dac08493efa
dhtt <dhthutrang@gmail.com>
2022-03-10 16:49:58 +0100

    Set pipeline default options, Add reset button & Make report_gene_abundance induce output_major_transcripts

--- src/main/webapp/js/functionality.js
+++ src/main/webapp/js/functionality.js
@@ -1,7 +1,38 @@
 // TODO: Show ResultSummary + NetworkViualization only after Running Progress is done
 // TODO: Check if progress shown while running
-
 jQuery(document).ready(function() {
+    // Set default
+    $.fn.set_default = function(){
+        $('#remove_decay_transcripts').prop('checked', true)
+        $('#threshold').val(1.0)
+    }
+    $.fn.set_default()
+
+    // Make output_major_transcripts true when report_gene_abundance
+    $.fn.make_all_checked = function(source_, target_){
+        const parent = $('#' + source_)
+        const children = $('#' + target_)
+        if (parent.is(':checked') === true){
+            children.prop("checked", true)
+        } else {
+            children.prop("checked", false)
+        }
+    }
+    $('#report_gene_abundance').on('click', function(){
+        $.fn.make_all_checked('report_gene_abundance','output_major_transcripts')
+    })
+
+    // Reset button
+    $.fn.make_none_checked = function (name){
+        $('[name="'+name+'"]').prop('checked', false)
+    }
+    $('.reset').on('click', function(){
+        const name = $(this).attr('id').split('Reset').join("");
+        $.fn.make_none_checked(name)
+        $.fn.set_default()
+    })
+
+    // Show number of uploaded files
     $('#protein_network_file').on("change", function(){
         $('#protein_network_file_lab').html("Change file")
         $('#protein_network_description').html("Protein interaction data: " + this.files.length + " file(s) selected")
@@ -29,6 +60,40 @@ jQuery(document).ready(function() {
         return false;
     })
 });
+
+
+// $.ajax({
+//     xhr: function () {
+//         var xhr = new window.XMLHttpRequest();
+//         xhr.upload.addEventListener("progress", function (evt) {
+//             if (evt.lengthComputable) {
+//                 var percentComplete = evt.loaded / evt.total;
+//                 console.log(percentComplete);
+//                 $('.progress').css({
+//                     width: percentComplete * 100 + '%'
+//                 });
+//                 if (percentComplete === 1) {
+//                     $('.progress').addClass('hide');
+//                 }
+//             }
+//         }, false);
+//         xhr.addEventListener("progress", function (evt) {
+//             if (evt.lengthComputable) {
+//                 var percentComplete = evt.loaded / evt.total;
+//                 console.log(percentComplete);
+//                 $('.progress').css({
+//                     width: percentComplete * 100 + '%'
+//                 });
+//             }
+//         }, false);
+//         return xhr;
+//     },
+//     type: 'POST',
+//     url: "/echo/html",
+//     data: data,
+//     success: function (data) {}
+// });
+
 
 function toggle1(name, displayTabs, chosenTab, chosenTab_contents){
     for (let i=0; i < displayTabs.length; i++){
